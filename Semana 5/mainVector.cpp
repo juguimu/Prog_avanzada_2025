3deec274b2d43328a731b2713e4cab30c6d5592f
Juan Muñoz <juangmunozc@gmail.com>
2025-03-07 06:58:20 -0500

    Semana 5 p2 fixed

--- Semana 5/mainVector.cpp
+++ Semana 5/mainVector.cpp
@@ -15,3 +15,36 @@ int main(){
     curso.mostrar();
     return 0;
 }
+
+//Algoritmo Eliminar
+/* 
+34 Pos 0
+23 Pos 1
+22 Pos 2
+21 Pos 3 <---
+
+
+34 Pos 0
+0  Pos 1
+22 Pos 2
+21 Pos 3 <---
+
+//paso 1
+34 Pos 0
+22 Pos 1
+0  Pos 2
+21 Pos 3 <---
+
+//Paso 2
+34 Pos 0
+22 Pos 1
+21 Pos 2
+0  Pos 3 <---
+
+//tam_vector--
+
+//Paso 3
+34 Pos 0
+22 Pos 1
+21 Pos 2 <---
+0  Pos 3  */
